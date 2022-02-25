95e266e32e03219c47b0713f7df807f6fdae78d7
drueda-pricesmart <drueda@pricesmart.com>
2022-02-25 10:26:54 -0400

    primero commit

---
+++ dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: 'dashboard.component.html'
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    let profile = JSON.parse(localStorage.getItem('session'));
+
+    if(profile.roles.indexOf('ADMIN') !== -1){
+      this.router.navigate(['pages/user-list']);
+    }else if(profile.roles.indexOf('MASTER') !== -1){
+      this.router.navigate(['pages/admin-list']);
+    }else{
+      if(profile.roles.indexOf('MACHINE_OWNER') !== -1){
+        this.router.navigate(['pages/products-list']);
+      }else{
+        if(!profile.emailVerified)
+          this.router.navigate(['email/verify/131qwe3434dadesam35o324sdf565jo']);
+        else  
+          this.router.navigate(['dashboard/info']);
+      }      
+    }
+  }  
+}
---
+++ dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { VerifyComponent } from './verify/verify.component';
+import { InfoComponent } from './info/info.component';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    DashboardRoutingModule,
+    ChartsModule,
+    BsDropdownModule,
+    ButtonsModule.forRoot()
+  ],
+  declarations: [ DashboardComponent, VerifyComponent, InfoComponent ]
+})
+export class DashboardModule { }
