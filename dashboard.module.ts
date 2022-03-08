ef045f87644600c31425e98b5502f2416b9450e2
drueda-pricesmart <drueda@pricesmart.com>
2022-03-08 16:33:17 -0400

    cambios para feture 1 2022

--- dashboard.module.ts
+++ dashboard.module.ts
@@ -12,7 +12,7 @@ import { InfoComponent } from './info/info.component';
 //este modulo cambiara
 //camio para b3
 //cambio 4 demo
-// cambio segundo b4 el cambio se cambio
+// cambio demooooooooooo b4 el cambio se cambio
 
 //demooo
 
@@ -22,6 +22,7 @@ import { InfoComponent } from './info/info.component';
     DashboardRoutingModule,
     ChartsModule,
     BsDropdownModule,
+	BsDropdownModule,
     ButtonsModule.forRoot()
   ],
   declarations: [ DashboardComponent, VerifyComponent, InfoComponent ]
